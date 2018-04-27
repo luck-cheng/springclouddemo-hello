96c1acdcdd7811ce10b70c29b8288cf7ff07889a
Reebok_139 <909891736@qq.com>
2018-04-27 17:26:15 +0800

    更新配置，还有test测试里面的内容，防止gradle打包的时候test出错

--- src/main/java/com/springclouddemo/hello/controller/HelloController.java
+++ src/main/java/com/springclouddemo/hello/controller/HelloController.java
@@ -1,6 +1,5 @@
 package com.springclouddemo.hello.controller;
 
-import com.sun.istack.internal.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,15 +11,13 @@ import java.util.List;
 @RestController
 public class HelloController {
 
-    private final Logger logger = Logger.getLogger(getClass());
-
     @Autowired
     private DiscoveryClient discoveryClient;
 
     @GetMapping("/hello")
     public String index(){
-        List<String> services = discoveryClient.getServices();
-        System.out.println(services);
+//        List<String> services = discoveryClient.getServices();
+//        System.out.println(services);
         return "Hello World";
     }
 }
